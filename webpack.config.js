e48daaf3ba392c0680ef19146565610e8538675c
laurieou <laurieou@gmail.com>
2016-03-28 16:07:21 -0700

    deleted extra files

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,8 @@ module.exports = {
   context: __dirname,
   entry: "./frontend/OrganGrinder.jsx",
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    path: "./",
     filename: "bundle.js",
-    devtoolModuleFilenameTemplate: '[resourcePath]',
-    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
   },
   module: {
     loaders: [
